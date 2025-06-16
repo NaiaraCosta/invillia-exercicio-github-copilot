94dbde2e13ecb881f27aa520568de1ffe370465d
NaiaraCosta <naiaradecosta@hotmail.com>
2025-06-16 20:57:52 +0000

    Adiciona exibição de participantes nas atividades e estiliza a lista de participantes

--- src/static/app.js
+++ src/static/app.js
@@ -26,6 +26,11 @@ document.addEventListener("DOMContentLoaded", () => {
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
           <p><strong>Participants:</strong> ${
+            <div class "participants-list"> {
+              constructor(parameters) {
+                
+              }
+            }
             details.participants.length > 0 
           ? details.participants.join(", ")
             : "No participants yet"
